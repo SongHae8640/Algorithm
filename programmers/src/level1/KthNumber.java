6848bbd3b3f5ea452dc8effa2f315d7bd76124e4
SongHae <thdgo456@gmail.com>
2021-09-30 00:15:13 +0900

    K번째 수 풀이 개선

--- programmers/src/level1/KthNumber.java
+++ programmers/src/level1/KthNumber.java
@@ -1,7 +1,6 @@
 package level1;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public class KthNumber {
     public static void main(String[] args) {
@@ -13,15 +12,9 @@ public class KthNumber {
 
 
         for (int i = 0; i < commands.length; i++) {
-            List<Integer> list = new ArrayList();
-            for (int j = commands[i][0]; j <= commands[i][1] ; j++) {
-                list.add(array[j-1]);
-
-            }
-            list.sort( (a, b) -> a.compareTo( b ) );
-
-
-            answer[i] = list.get(commands[i][2] -1);
+            int[] cutArray = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            Arrays.sort(cutArray);
+            answer[i] = cutArray[commands[i][2] -1];
         }
 
         return answer;
@@ -32,13 +25,13 @@ public class KthNumber {
  * https://programmers.co.kr/learn/courses/30/lessons/42748
  *
  * 정확성  테스트
- * 테스트 1 〉	통과 (0.50ms, 72.4MB)
- * 테스트 2 〉	통과 (0.72ms, 76.2MB)
- * 테스트 3 〉	통과 (0.62ms, 72MB)
- * 테스트 4 〉	통과 (0.52ms, 73.3MB)
- * 테스트 5 〉	통과 (0.67ms, 72MB)
- * 테스트 6 〉	통과 (0.57ms, 74.1MB)
- * 테스트 7 〉	통과 (0.56ms, 72.5MB)
+ * 테스트 1 〉	통과 (0.50ms, 73.1MB)
+ * 테스트 2 〉	통과 (0.32ms, 73.1MB)
+ * 테스트 3 〉	통과 (0.43ms, 75.8MB)
+ * 테스트 4 〉	통과 (0.34ms, 71.9MB)
+ * 테스트 5 〉	통과 (0.50ms, 75.9MB)
+ * 테스트 6 〉	통과 (0.32ms, 75.4MB)
+ * 테스트 7 〉	통과 (0.35ms, 70.8MB)
  * 채점 결과
  * 정확성: 100.0
  * 합계: 100.0 / 100.0
