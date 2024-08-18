fee8af91fae8ed23e8bfa5e2e5e8bdb11849384c
SongHae8640 <thdgo456@gmail.com>
2024-08-18 09:31:50 +0900

    이중 우선순위 큐 풀이 - 프로그래머스

--- programmers/src/level3/DoublePriorityQueue.java
+++ programmers/src/level3/DoublePriorityQueue.java
@@ -1,52 +1,52 @@
 package level3;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.TreeSet;
 
 public class DoublePriorityQueue {
+    public static void main(String[] args) {
+        // ["I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"] => [0, 0]
+        String[] operations = new String[]{"I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"};
+        int[] solution = new DoublePriorityQueue().solution(operations);
+        display(solution);
 
-	public static void main(String[] args) {
-        //solution(new String[] {"I 16","D 1"});
-        //solution(new String[] {"I 7","I 5","I -5","D -1"});
-        solution(new String[] {"I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"});
-
-	}
-	
-    public static int[] solution(String[] operations) {
-    	int[] answer = {};
-    	ArrayList<Integer> aList = new ArrayList<Integer>();
-    	for (int i = 0; i < operations.length; i++) {
-			if(operations[i].contains("I")) {
-				aList.add(new Integer(operations[i].split(" ")[1]));
-			}else {
-				//정렬
-				aList.sort(new Comparator<Integer>() {
-					@Override
-					public int compare(Integer o1, Integer o2) {
-						// TODO Auto-generated method stub
-						return o1-o2;
-					}
-				});
-				System.out.println(aList.toString());
-				
-				//삭제
-				if(new Integer(operations[i].split(" ")[1]) ==1) {
-					aList.remove(aList.size()-1);
-				}else {
-					aList.remove(0);
-				}
-				
-			}
-		}
-    	
-    	
-    	if(aList.size()==0) {
-    		return new int[] {0,0};
-    	}else if(aList.size()==1) {
-    		return new int[] {aList.get(0), aList.get(0)};
-    	}
-        return new int[] {aList.get(aList.size()-1), aList.get(0)};
+        // ["I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"] => [333, -45]
+        operations = new String[]{"I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"};
+        solution = new DoublePriorityQueue().solution(operations);
+        display(solution);
     }
 
+    private static void display(int[] solution) {
+        for (int i : solution) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+
+    public int[] solution(String[] operations) {
+        TreeSet<Integer> treeSet = new TreeSet<>();
+        for (String operation : operations){
+            if(isInsertOperation(operation)){
+                String [] split = operation.split(" ");
+                treeSet.add(Integer.parseInt(split[1]));
+            } else if(isDeleteMaxOperation(operation)) {
+                treeSet.pollLast();
+            } else if(isDeleteMinOperation(operation)) {
+                treeSet.pollFirst();
+            }
+        }
+        return new int[]{treeSet.isEmpty() ? 0 : treeSet.last(), treeSet.isEmpty() ? 0 : treeSet.first()};
+    }
+
+    private boolean isInsertOperation(String operation) {
+        return operation.startsWith("I");
+    }
+
+    private boolean isDeleteMaxOperation(String operation) {
+        return operation.equals("D 1");
+    }
+
+    private boolean isDeleteMinOperation(String operation) {
+        return operation.equals("D -1");
+    }
 }
